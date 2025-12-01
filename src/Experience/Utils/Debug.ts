fc05df14cc0fd369ccef9dcf857d174635a40775
hayk <haykh.astro@gmail.com>
2025-11-30 21:20:42 -0500

    DebugQuad for GPGPU

--- src/Experience/Utils/Debug.ts
+++ src/Experience/Utils/Debug.ts
@@ -1,4 +1,15 @@
-import { CanvasTexture } from "three";
+import {
+  CanvasTexture,
+  Scene,
+  OrthographicCamera,
+  PlaneGeometry,
+  Mesh,
+  MeshBasicMaterial,
+  Texture,
+  Vector2,
+  Vector4,
+  WebGLRenderer,
+} from "three";
 import { GUI } from "three/examples/jsm/libs/lil-gui.module.min";
 
 export default class Debug {
@@ -51,9 +62,88 @@ export class DebugCanvas {
 
     this.context = this.instance.getContext("2d")!;
     this.texture = new CanvasTexture(this.instance);
+
+    this.context.fillRect(0, 0, this.instance.width, this.instance.height);
   }
 
   update() {
     this.texture.needsUpdate = true;
+  }
+}
+
+export class DebugQuad {
+  private renderer: WebGLRenderer;
+  private scene: Scene;
+  private camera: OrthographicCamera;
+  private mesh: Mesh;
+  private texture: Texture | null = null;
+
+  private heightFraction: number;
+
+  constructor(renderer: WebGLRenderer, heightFraction: number = 0.3) {
+    this.renderer = renderer;
+    this.heightFraction = heightFraction;
+
+    this.scene = new Scene();
+    this.camera = new OrthographicCamera(-1, 1, 1, -1, 0, 1);
+
+    const geom = new PlaneGeometry(2, 2);
+    const mat = new MeshBasicMaterial();
+
+    this.mesh = new Mesh(geom, mat);
+    this.scene.add(this.mesh);
+  }
+
+  setTexture(tex: Texture) {
+    this.texture = tex;
+    (this.mesh.material as MeshBasicMaterial).map = tex;
+    (this.mesh.material as MeshBasicMaterial).needsUpdate = true;
+  }
+
+  render() {
+    if (this.texture === null) return;
+    const size = new Vector2();
+    this.renderer.getSize(size);
+    const viewportHeight = size.y;
+
+    const h = viewportHeight * this.heightFraction;
+
+    const texAny = this.texture as any;
+    const texWidth = texAny.width ?? 1;
+    const texHeight = texAny.height ?? 1;
+    const texAspect = texWidth / texHeight;
+
+    const w = h * texAspect;
+
+    const x = 0;
+    const y = viewportHeight - h;
+
+    const prevViewport = this.renderer.getViewport(new Vector4());
+    const prevScissor = this.renderer.getScissor(new Vector4());
+    const prevScissorTest = this.renderer.getScissorTest();
+    const prevAutoClear = this.renderer.autoClear;
+
+    this.renderer.setViewport(x, y, w, h);
+    this.renderer.setScissor(x, y, w, h);
+    this.renderer.setScissorTest(true);
+
+    this.renderer.autoClear = false;
+    this.renderer.clearDepth();
+    this.renderer.render(this.scene, this.camera);
+
+    this.renderer.setViewport(
+      prevViewport.x,
+      prevViewport.y,
+      prevViewport.z,
+      prevViewport.w,
+    );
+    this.renderer.setScissor(
+      prevScissor.x,
+      prevScissor.y,
+      prevScissor.z,
+      prevScissor.w,
+    );
+    this.renderer.setScissorTest(prevScissorTest);
+    this.renderer.autoClear = prevAutoClear;
   }
 }
